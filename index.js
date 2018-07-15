fbd5af93a924192ca9800de06d937e21d3ff9553
Roqak <Roqak1@gmail.com>
2018-07-15 12:20:37 +0100

    fff

--- index.js
+++ index.js
@@ -18,12 +18,12 @@ mongoose.connect(mongoUri)
 
 app.use(passport.initialize());
 require('./Routes/auth')(passport);
-// if(process.env.NODE_ENV==='production'){
-//     app.use(express.static('client/build'));
-//     app.get('*',(req,res)=>{
-//         res.sendfile(path.resolve(__dirname,'client','build','index.html'))
-//     })
-// }
+if(process.env.NODE_ENV==='production'){
+    app.use(express.static('client/build'));
+    app.get('*',(req,res)=>{
+        res.sendfile(path.resolve(__dirname,'client','build','index.html'))
+    })
+}
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
